e402656e1bf779e729419fd6bd3ed7a90162174e
15235758948 <15235758948@163.com>
2017-04-18 19:05:07 +0800

    写了四个Fragment的底部导航和一个小布局

---
+++ app/src/main/java/com/example/administrator/a18master/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.a18master;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ding1);
+    }
+}

def8259fcc3b9ddc43788e1f66b2ff8c667d8a98
15235758948 <15235758948@163.com>
2017-04-19 19:15:17 +0800

    添加了home页面

--- app/src/main/java/com/example/administrator/a18master/MainActivity.java
+++ app/src/main/java/com/example/administrator/a18master/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.administrator.a18master;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.example.administrator.a18master.base.banner.BannerAdapter;
+import com.example.administrator.a18master.base.banner.BannerLayout;
+import com.example.administrator.a18master.base.banner.LocalBanner;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends FragmentActivity {
 
@@ -24,6 +39,19 @@ public class MainActivity extends FragmentActivity {
     RadioButton rbMe;
     @BindView(R.id.tab_menu)
     RadioGroup tabMenu;
+    @BindView(R.id.layout_banner)
+    BannerLayout layoutBanner;
+    @BindView(R.id.layout_banners)
+    BannerLayout layoutBanners;
+    @BindView(R.id.layout_banner_shequ)
+    ViewPager layoutBannerShequ;
+    @BindView(R.id.home_xyd_2)
+    ImageView homeXyd2;
+    @BindView(R.id.home_xyd_1)
+    ImageView homeXyd1;
+    @BindView(R.id.home_xyd_3)
+    ImageView homeXyd3;
+
     private HomeFragment homeFragment;
     private ShopFragment shopFragment;
     private FocusFragment focusFragment;
@@ -35,14 +63,22 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        if (getActionBar() != null) {
+            getActionBar().hide();
+        }
 //        底部导航+Fragment切换
+        init();
+//       轮播
+        initBanner();
+        initBanners();
         initView();
 
     }
 
-    public void initView() {
+    public void init() {
         homeFragment = new HomeFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, homeFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, homeFragment)
+                .commit();
         myTabRg = (RadioGroup) findViewById(R.id.tab_menu);
         myTabRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -59,7 +95,8 @@ public class MainActivity extends FragmentActivity {
                             shopFragment = new ShopFragment();
                         }
                         Log.i("MyFragment", "FragmentAddress");
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, shopFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, shopFragment)
+                                .commit();
                         break;
                     case R.id.rbFind:
                         focusFragment = new FocusFragment();
@@ -77,5 +114,119 @@ public class MainActivity extends FragmentActivity {
 
             }
         });
+    }
+
+    // 轮播图
+    private void initBanner() {
+
+        BannerAdapter<LocalBanner> bannerAdapter = new BannerAdapter<LocalBanner>() {
+            @Override
+            protected void bind(ViewHolder holder, LocalBanner data) {
+                Picasso.with(getApplicationContext()).load(data.getLocalRes()).into(holder.mImageView);
+            }
+        };
+        layoutBanner.setAdapter(bannerAdapter);
+
+        List<LocalBanner> localBanners = new ArrayList<>();
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        bannerAdapter.reset(localBanners);
+
+    }
+
+    private void initBanners() {
+
+        BannerAdapter<LocalBanner> bannerAdapter = new BannerAdapter<LocalBanner>() {
+            @Override
+            protected void bind(ViewHolder holder, LocalBanner data) {
+                Picasso.with(getApplicationContext()).load(data.getLocalRes()).into(holder.mImageView);
+            }
+        };
+        layoutBanners.setAdapter(bannerAdapter);
+
+        List<LocalBanner> localBanners = new ArrayList<>();
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        localBanners.add(new LocalBanner(R.drawable.banner));
+        bannerAdapter.reset(localBanners);
+
+    }
+
+//    初始化视图
+    private void initView() {
+        //viewPager适配器
+        layoutBannerShequ.setAdapter(adapter);
+        //viewPager监听->Button的切换
+        layoutBannerShequ.addOnPageChangeListener(listener);
+        //首次进入默认选中在线新闻btn
+        homeXyd1.setSelected(true);
+    }
+
+//    viewPager监听->Button的切换
+    private ViewPager.OnPageChangeListener listener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            //Button，UI改变
+            homeXyd1.setSelected(position == 0);
+            homeXyd2.setSelected(position == 1);
+            homeXyd3.setSelected(position == 2);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    //viewPager适配器
+    private FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+        @Override
+        public int getCount() {
+            //一共三页，写死3
+            return 3;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new HomeFragment();
+                case 1:
+                    return new HomeFragment();
+                case 2:
+                    return new HomeFragment();
+                default:
+                    throw new RuntimeException("未知错误");
+            }
+        }
+
+    };
+
+    @OnClick({R.id.home_xyd_2, R.id.home_xyd_1, R.id.home_xyd_3})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.home_xyd_1:
+                //不要平滑效果，第二个参数传false
+                layoutBannerShequ.setCurrentItem(0, false);
+                break;
+            case R.id.home_xyd_2:
+                //不要平滑效果，第二个参数传false
+                layoutBannerShequ.setCurrentItem(1, false);
+                break;
+            case R.id.home_xyd_3:
+                //不要平滑效果，第二个参数传false
+                layoutBannerShequ.setCurrentItem(2, false);
+                break;
+            default:
+                throw new RuntimeException("未知错误");
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/a18master/base/banner/LocalBanner.java
@@ -0,0 +1,22 @@
+package com.example.administrator.a18master.base.banner;
+
+/**
+ * Created by Administrator on 2017/4/19.
+ */
+
+public class LocalBanner {
+
+    public LocalBanner(int localRes) {
+        this.localRes = localRes;
+    }
+
+    private int localRes;
+
+    public int getLocalRes() {
+        return localRes;
+    }
+
+    public void setLocalRes(int localRes) {
+        this.localRes = localRes;
+    }
+}

eed64ae6b69a141e7a132214810762d7f6615166
15235758948 <15235758948@163.com>
2017-04-19 12:09:54 +0800

    修改了底部导航

--- app/src/main/java/com/example/administrator/a18master/MainActivity.java
+++ app/src/main/java/com/example/administrator/a18master/MainActivity.java
@@ -1,204 +1,82 @@
 package com.example.administrator.a18master;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
+import android.util.Log;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends FragmentActivity{
+public class MainActivity extends FragmentActivity {
 
-    @BindView(R.id.title_imv)
-    ImageView titleLeftImv;
-    @BindView(R.id.title_text_tv)
-    TextView titleTv;
-    @BindView(R.id.content)
-    FrameLayout content;
-    @BindView(R.id.first_image)
-    ImageView firstImage;
-    @BindView(R.id.first_text)
-    TextView firstText;
-    @BindView(R.id.first_layout)
-    RelativeLayout firstLayout;
-    @BindView(R.id.second_image)
-    ImageView secondImage;
-    @BindView(R.id.second_text)
-    TextView secondText;
-    @BindView(R.id.second_layout)
-    RelativeLayout secondLayout;
-    @BindView(R.id.third_image)
-    ImageView thirdImage;
-    @BindView(R.id.third_text)
-    TextView thirdText;
-    @BindView(R.id.third_layout)
-    RelativeLayout thirdLayout;
-    @BindView(R.id.fourth_image)
-    ImageView fourthImage;
-    @BindView(R.id.fourth_text)
-    TextView fourthText;
-    @BindView(R.id.fourth_layout)
-    RelativeLayout fourthLayout;
-    // 定义4个Fragment对象
-    private HomeFragment fg1;
-    private ShopFragment fg2;
-    private FocusFragment fg3;
-    private MyFragment fg4;
-    // 定义每个选项中的相关控件
-    // 定义几个颜色
-    private int whirt = 0xFFFFFFFF;
-    private int gray = 0xFF7597B3;
-    private int dark = 0xff000000;
-    // 定义FragmentManager对象管理器
-    private FragmentManager fragmentManager;
+    @BindView(R.id.main_content)
+    FrameLayout mainContent;
+    @BindView(R.id.rbChat)
+    RadioButton rbChat;
+    @BindView(R.id.rbAddress)
+    RadioButton rbAddress;
+    @BindView(R.id.rbFind)
+    RadioButton rbFind;
+    @BindView(R.id.rbMe)
+    RadioButton rbMe;
+    @BindView(R.id.tab_menu)
+    RadioGroup tabMenu;
+    private HomeFragment homeFragment;
+    private ShopFragment shopFragment;
+    private FocusFragment focusFragment;
+    private MyFragment myFragment;
+    private RadioGroup myTabRg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        fragmentManager = getSupportFragmentManager();
-        initView(); // 初始化界面控件
-        setChioceItem(0); // 初始化页面加载时显示第一个选项卡
+        initView();
+
     }
 
-    /**
-     * 初始化页面
-     */
-    private void initView() {
-// 初始化页面标题栏
-        titleLeftImv.setOnClickListener(new View.OnClickListener() {
+    public void initView() {
+        homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, homeFragment).commit();
+        myTabRg = (RadioGroup) findViewById(R.id.tab_menu);
+        myTabRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // TODO Auto-generated method stub
+                switch (checkedId) {
+                    case R.id.rbChat:
+                        homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, homeFragment)
+                                .commit();
+                        break;
+                    case R.id.rbAddress:
+                        if (shopFragment == null) {
+                            shopFragment = new ShopFragment();
+                        }
+                        Log.i("MyFragment", "FragmentAddress");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, shopFragment).commit();
+                        break;
+                    case R.id.rbFind:
+                        focusFragment = new FocusFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, focusFragment)
+                                .commit();
+                        break;
+                    case R.id.rbMe:
+                        myFragment = new MyFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, myFragment)
+                                .commit();
+                        break;
+                    default:
+                        break;
+                }
+
             }
         });
-        titleTv.setText("首 页");
-    }
-
-    /**
-     * 设置点击选项卡的事件处理
-     *
-     * @param index 选项卡的标号：0, 1, 2, 3
-     */
-    private void setChioceItem(int index) {
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        clearChioce(); // 清空, 重置选项, 隐藏所有Fragment
-        hideFragments(fragmentTransaction);
-        switch (index) {
-            case 0:
-// firstImage.setImageResource(R.drawable.XXXX); 需要的话自行修改
-                firstText.setTextColor(dark);
-                firstLayout.setBackgroundColor(gray);
-// 如果fg1为空，则创建一个并添加到界面上
-                if (fg1 == null) {
-                    fg1 = new HomeFragment();
-                    fragmentTransaction.add(R.id.content, fg1);
-                } else {
-// 如果不为空，则直接将它显示出来
-                    fragmentTransaction.show(fg1);
-                }
-                break;
-            case 1:
-// secondImage.setImageResource(R.drawable.XXXX);
-                secondText.setTextColor(dark);
-                secondLayout.setBackgroundColor(gray);
-                if (fg2 == null) {
-                    fg2 = new ShopFragment();
-                    fragmentTransaction.add(R.id.content, fg2);
-                } else {
-                    fragmentTransaction.show(fg2);
-                }
-                break;
-            case 2:
-// thirdImage.setImageResource(R.drawable.XXXX);
-                thirdText.setTextColor(dark);
-                thirdLayout.setBackgroundColor(gray);
-                if (fg3 == null) {
-                    fg3 = new FocusFragment();
-                    fragmentTransaction.add(R.id.content, fg3);
-                } else {
-                    fragmentTransaction.show(fg3);
-                }
-                break;
-            case 3:
-// fourthImage.setImageResource(R.drawable.XXXX);
-                fourthText.setTextColor(dark);
-                fourthLayout.setBackgroundColor(gray);
-                if (fg4 == null) {
-                    fg4 = new MyFragment();
-                    fragmentTransaction.add(R.id.content, fg4);
-                } else {
-                    fragmentTransaction.show(fg4);
-                }
-                break;
-        }
-        fragmentTransaction.commit(); // 提交
-    }
-
-    /**
-     * 当选中其中一个选项卡时，其他选项卡重置为默认
-     */
-    private void clearChioce() {
-// firstImage.setImageResource(R.drawable.XXX);
-        firstText.setTextColor(gray);
-        firstLayout.setBackgroundColor(whirt);
-// secondImage.setImageResource(R.drawable.XXX);
-        secondText.setTextColor(gray);
-        secondLayout.setBackgroundColor(whirt);
-// thirdImage.setImageResource(R.drawable.XXX);
-        thirdText.setTextColor(gray);
-        thirdLayout.setBackgroundColor(whirt);
-// fourthImage.setImageResource(R.drawable.XXX);
-        fourthText.setTextColor(gray);
-        fourthLayout.setBackgroundColor(whirt);
-    }
-
-    /**
-     * 隐藏Fragment
-     *
-     * @param fragmentTransaction
-     */
-    private void hideFragments(FragmentTransaction fragmentTransaction) {
-        if (fg1 != null) {
-            fragmentTransaction.hide(fg1);
-        }
-        if (fg2 != null) {
-            fragmentTransaction.hide(fg2);
-        }
-        if (fg3 != null) {
-            fragmentTransaction.hide(fg3);
-        }
-        if (fg4 != null) {
-            fragmentTransaction.hide(fg4);
-        }
-    }
-
-    @OnClick({R.id.first_layout, R.id.second_layout, R.id.third_layout, R.id.fourth_layout})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.first_layout:
-                setChioceItem(0);
-                break;
-            case R.id.second_layout:
-                setChioceItem(1);
-                break;
-            case R.id.third_layout:
-                setChioceItem(2);
-                break;
-            case R.id.fourth_layout:
-                setChioceItem(3);
-                break;
-            default:
-                break;
-        }
     }
 }

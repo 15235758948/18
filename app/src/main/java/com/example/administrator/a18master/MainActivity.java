db874cd31cb1fa5a0f12dbb5270ce7516fcfb6e1
15235758948 <15235758948@163.com>
2017-05-05 16:56:09 +0800

    调了一些页面字体，写了添加关注页面的跳转等

--- app/src/main/java/com/example/administrator/a18master/MainActivity.java
+++ app/src/main/java/com/example/administrator/a18master/MainActivity.java
@@ -11,13 +11,16 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.administrator.a18master.my.CachePreferences;
+import com.example.administrator.a18master.utils.ActivityUtils;
 import com.example.popmenu.PopupMenuView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     @BindView(R.id.main_content)
     FrameLayout mainContent;
@@ -38,12 +41,16 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private MyFragment myFragment;
     private RadioGroup myTabRg;
     private PopupMenuView mCustomMenuView;
+    //点击2次返回，退出程序
+    private int mBackKeyPressedTimes = 0;
+//    private ActivityUtils activityUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+//        activityUtils= new ActivityUtils(this);
         if (getActionBar() != null) {
             getActionBar().hide();
         }
@@ -78,19 +85,29 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                         if (shopFragment == null) {
                             shopFragment = new ShopFragment();
                         }
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, shopFragment)
-                                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+                        if (CachePreferences.getUser().getName() == null) {
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.main_content, shopFragment)
+                                    .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+                        }
                         break;
                     case R.id.rbFind:
-                        focusFragment = new FocusFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, focusFragment)
-                                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+                        if (CachePreferences.getUser().getName() == null) {
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            focusFragment = new FocusFragment();
+                            getSupportFragmentManager().beginTransaction().replace(R.id.main_content, focusFragment)
+                                    .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+                        }
                         break;
                     case R.id.rbMe:
-                        if (CachePreferences.getUser().getName() == null){
-                            Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                        if (CachePreferences.getUser().getName() == null) {
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                             startActivity(intent);
-                        }else{
+                        } else {
                             myFragment = new MyFragment();
                             getSupportFragmentManager().beginTransaction().replace(R.id.main_content, myFragment)
                                     .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
@@ -102,7 +119,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             }
         });
     }
-//    wx.feicuiedu.com:9094/yitao
 
     @Override
     public void onClick(View v) {
@@ -111,6 +127,31 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 //                mPopupMenuView.setOrientation(LinearLayout.VERTICAL);
                 mCustomMenuView.setOrientation(LinearLayout.VERTICAL);
                 mCustomMenuView.show(v);
-            break;
-        }}
+                break;
+        }
+    }
+    //点击两次返回退出程序
+    @Override
+    public void onBackPressed() {
+        if (mBackKeyPressedTimes == 0) {
+            Toast.makeText(this, "再按一次退出程序 ", Toast.LENGTH_SHORT).show();
+            mBackKeyPressedTimes = 1;
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        mBackKeyPressedTimes = 0;
+                    }
+                }
+            }.start();
+            return;
+        } else {
+           finish();
+        }
+        super.onBackPressed();
+    }
 }
